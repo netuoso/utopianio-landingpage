4a0f8742bd19a359c2ef745af5001c0401d4de3a
Andrew Chaney (netuoso) <andrewc@pobox.com>
2017-11-02 16:54:05 -0500

    init repo

---
+++ Gemfile
@@ -0,0 +1,36 @@
+source 'https://rubygems.org'
+
+git_source(:github) do |repo_name|
+  repo_name = "#{repo_name}/#{repo_name}" unless repo_name.include?("/")
+  "https://github.com/#{repo_name}.git"
+end
+
+gem 'rails', '~> 5.1.4'
+gem 'puma', '~> 3.7'
+gem 'sass-rails', '~> 5.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.2'
+
+gem 'jquery-rails'
+gem 'bootstrap-sass'
+gem 'jquery-slick-rails'
+gem 'font-awesome-rails'
+
+gem 'nokogiri'
+gem 'rest-client'
+
+group :productiotn do
+  gem 'pg'
+end
+
+group :development, :test do
+  gem 'pry-byebug'
+  gem 'sqlite3'
+end
+
+group :development do
+  gem 'web-console', '>= 3.3.0'
+  gem 'listen', '>= 3.0.5', '< 3.2'
+end
+
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ app/assets/javascripts/js/index.js
@@ -0,0 +1,84 @@
+$(function () {
+  // init scroll animations
+  let scrollController = new ScrollMagic.Controller();
+
+  // nav
+  $(window).on('scroll', function () {
+    let nav = $('#navbar');
+    if ($(this).scrollTop() > 150) {
+      nav.addClass('scrolled');
+    } else {
+      nav.removeClass('scrolled');
+    }
+  });
+  $(window).scroll();
+
+  // waves
+  let waveWhite = new TimelineMax();
+  waveWhite.to($('#wave-white'), 20, {backgroundPositionX:-300, repeatDelay:0, repeat:-1, yoyo:true, ease: Power1.easeInOut});
+  waveWhite.play();
+
+  let waveGrey1 = new TimelineMax();
+  waveGrey1.to($('#wave-grey1'), 30, {backgroundPositionX:300, repeatDelay:0, repeat:-1, yoyo:true, ease: Power1.easeInOut});
+  waveGrey1.play();
+
+  let waveGrey2 = new TimelineMax();
+  waveGrey2.to($('#wave-grey2'), 40, {backgroundPositionX:-250, repeatDelay:0, repeat:-1, yoyo:true, ease: Power1.easeInOut});
+  waveGrey2.play();
+
+  // github
+  let octocatTween = new TweenMax.from('#octocat', 1, {
+    scale: 0,
+    opacity: 0,
+    ease: Back.easeInOut
+  });
+
+  let octocatScene = new ScrollMagic.Scene({
+    triggerElement: '#github-trigger',
+    offset: -300
+  })
+    .setTween(octocatTween)
+    .addTo(scrollController);
+
+  let textTween = new TweenMax.from('#github-text', 1, {
+    delay: .3,
+    scale: 0,
+    opacity: 0,
+    ease: Back.easeInOut
+  });
+
+  let githubTextScene = new ScrollMagic.Scene({
+    triggerElement: '#github-trigger',
+    offset: -300
+  })
+    .setTween(textTween)
+    .addTo(scrollController);
+
+  // topics
+  let topicTween = new TweenMax.staggerFrom('#topics .card', 1, {
+    opacity: 0,
+    top: -50,
+    ease: Back.easeInOut,
+    delay: 1
+  }, .05);
+
+  let topicsScene = new ScrollMagic.Scene({
+    triggerElement: '#github-trigger',
+    offset: '-50%'
+  })
+    .setTween(topicTween)
+    .addTo(scrollController);
+
+  let topicArrowsTween = new TweenMax.from('#topics .carousel-control-prev, #topics .carousel-control-next', 1, {
+    opacity: 0,
+    ease: Back.easeInOut,
+    delay: 1
+  });
+
+  let topicArrowsScene = new ScrollMagic.Scene({
+    triggerElement: '#github-trigger',
+    offset: '-50%'
+  })
+    .setTween(topicArrowsTween)
+    .addTo(scrollController);
+});
